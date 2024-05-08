6a3575b2b7ca3cb89f5bf6f6028f0131a47fb1b3
+ <+>
2024-05-08 08:00:29 +0300

    changes

---
+++ my-project/src/components/App.jsx
@@ -0,0 +1,36 @@
+
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./LayOut";
+import Contacts from "./pages/Contacts";
+import MyOrders from "./pages/MyOrders";
+import NoPage from "./pages/NoPage";
+import About from "./pages/About";
+import Menu from "./pages/Menu";
+import More from "./pages/More";
+import OnlineOrders from "./pages/OnlineOrders";
+
+function App() {
+
+  return (
+   
+     <BrowserRouter >
+    <Routes>
+      <Route path='/' element={<Layout />}> 
+      <Route path='/home' element={<Home />} />
+      <Route path='/contacts' element={<Contacts />} />
+      <Route path='/Myoders' element={<MyOrders />} />
+      <Route path="*" element={<NoPage />} /></Route>
+      <Route path='/about' element={<About />} />
+      <Route path='/menu' element={<Menu />} />
+      <Route path='/more' element={<More />} />
+      <Route path='/onlineorders' element={<OnlineOrders />} />
+    </Routes>
+    
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate';
 import { createRoot } from 'react-dom/client';
+import App from '../App'; 
 
 // Example items, to simulate fetching from another resources.
 const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
@@ -57,9 +58,15 @@ function PaginatedItems({ itemsPerPage }) {
 }
 
 // Add a <div id="container"> to your HTML to see the component rendered.
-const container = document.getElementById('app');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />); 
+const container = document.getElementById('App');
+const root = createRoot(document.getElementById("root")
+)// createRoot(container!) if you use TypeScript
+.render( 
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+);
+
   <PaginatedItems itemsPerPage={4} />,
   document.getElementById('container')
 
